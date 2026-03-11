4ad4a04fa866cbf856e4567709c9950f9da55a98
Emiliakawaiii <trantiendungxg@gmail.com>
2026-03-11 09:37:03 +0700

    Fix stbi_load on Windows for filesystem

--- src/image_loader.cpp
+++ src/image_loader.cpp
@@ -19,7 +19,8 @@ const ImageLoadResult& ImageLoader::getResult() const {
 ImageLoader ImageLoader::loadImageFromPath(const std::filesystem::path& path) {
     stbi_set_flip_vertically_on_load(true);
     ImageLoadResult res;
-    res.pixels = stbi_load(path.c_str(), &res.texWidth, &res.texHeight, &res.texChannels, STBI_rgb_alpha);
+    // filesystem::path::c_str() returns path::value_type*, which is OS-dependent (const wchar_t* on Windows vs const char* on Linux)
+    res.pixels = stbi_load(path.string().c_str(), &res.texWidth, &res.texHeight, &res.texChannels, STBI_rgb_alpha);
     if (!res.pixels) {
         throw std::runtime_error("Failed to load image from path: " + path.string() + '\n');
     }
