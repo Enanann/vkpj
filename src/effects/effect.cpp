d9b559adb44e47d1c801ae11e940583e16fb6394
Enanann <[trantiendungxg@gmail.com]>
2026-04-11 01:55:01 +0700

    Switch to constructing Effect with an EffectCreateInfo

--- src/effects/effect.cpp
+++ src/effects/effect.cpp
@@ -3,15 +3,56 @@
 #include "descriptor_layout.hpp"
 #include "device.hpp"
 
-Effect::Effect(VulkanDevice& device, const std::filesystem::path& path, DescriptorSetLayout& layout) {
+Effect::Effect(std::string_view name, VulkanDevice& device, const std::filesystem::path& path, DescriptorSetLayout& layout, const ComputePipelineConfig& config) 
+    : mName{name}
+{
+    mIsEnabled = true;
     mShader.emplace(device, path);
-    mPipeline.emplace(device, *mShader, layout);
+    mPipeline.emplace(device, *mShader, layout, config);
 }
+
+Effect::Effect(VulkanDevice& device, DescriptorSetLayout& layout, const EffectCreateInfo& createInfo) {
+    mIsEnabled = true;
+    mName = createInfo.name;
+    mShader.emplace(device, createInfo.path);
+    mPipeline.emplace(device, *mShader, layout, createInfo.config);
+
+    for (const auto& p : createInfo.params) {
+        this->addParam(p);
+    }
+}
+
 
 Shader& Effect::getShader() {
     return *mShader;
 } 
 
-ComputePipeline& Effect::getPipeline() {
+const ComputePipeline& Effect::getPipeline() const {
     return *mPipeline;
 }
+
+bool Effect::usePushConstant() {
+    return mPipeline->mUsePushConstant;
+}
+
+const std::string_view Effect::getName() const {
+    return mName;
+}
+
+std::vector<EffectParam>& Effect::getParams() {
+    return mParams;
+} 
+
+std::vector<float>& Effect::getParamsData() {
+    return mParamsData;
+} 
+
+uint32_t Effect::getFloatParamSize() {
+    return static_cast<uint32_t>(mParams.size()) * sizeof(float);
+}
+
+void Effect::addParam(EffectParam param) {
+    mParams.push_back(param);
+    mParamsData.push_back(param.defaultValue);
+}
+
--- src/effects/effect.hpp
+++ src/effects/effect.hpp
@@ -6,22 +6,42 @@
 
 #include <filesystem>
 #include <optional>
+#include <string_view>
 #include <vector>
 #include <string>
 
 class VulkanDevice;
 class DescriptorSetLayout;
 
+struct EffectCreateInfo {
+    std::string                 name;
+    const std::filesystem::path path;
+    ComputePipelineConfig       config;
+    std::vector<EffectParam>    params;
+};
+
 class Effect {
 public:
-    Effect(VulkanDevice&, const std::filesystem::path&, DescriptorSetLayout&);
+    bool mIsEnabled;
+
+    Effect(std::string_view, VulkanDevice&, const std::filesystem::path&, DescriptorSetLayout&, const ComputePipelineConfig&);
+    Effect(VulkanDevice&, DescriptorSetLayout&, const EffectCreateInfo&);
 
     Shader& getShader();
-    ComputePipeline& getPipeline();
+    const ComputePipeline& getPipeline() const;
+    bool usePushConstant();
+
+    const std::string_view getName() const;
+    std::vector<EffectParam>& getParams(); 
+    std::vector<float>& getParamsData(); 
+    uint32_t getFloatParamSize();
 private:
+    void addParam(EffectParam);
+
     std::string mName;
 
     std::optional<Shader> mShader;
     std::optional<ComputePipeline> mPipeline;
     std::vector<EffectParam> mParams;
+    std::vector<float>       mParamsData;
 };
